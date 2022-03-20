7bb621812563ca707dc905dcf7633a827c849648
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-03-20 20:12:49 +0200

    ...

--- index.js
+++ index.js
@@ -1,45 +1,21 @@
-const form = document.getElementById("form");
+// function validEmail() {
+//   var mail = document.getElementById("mail").value; // where `mail` is id of your input form
+//   var email = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
 
-const email = document.getElementById("email");
+//   if (mail.match(email)) {
+//     alert("Your Email : " + mail);
+//   } else {
+//     alert("Invalid Email !!!");
+//   }
+// }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  validateInputs();
-});
-
-const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-
-  errorDisplay.innerText = message;
-  inputControl.classList.add("error");
-  inputControl.classList.remove("success");
-};
-
-const setSuccess = (element) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector(".error");
-
-  errorDisplay.innerText = "";
-  inputControl.classList.add("success");
-  inputControl.classList.remove("error");
-};
-
-const isValidEmail = (email) => {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-};
-
-const validateInputs = () => {
-  const emailValue = email.value.trim();
-
-  if (emailValue === "") {
-    setError(email, "Email is required");
-  } else if (!isValidEmail(emailValue)) {
-    setError(email, "Provide a valid email address");
+function errorMessage() {
+  var error = document.getElementById("error");
+  if (isNaN(document.getElementById("number").value)) {
+    // Changing content and color of content
+    error.textContent = "Please enter a valid number";
+    error.style.color = "red";
   } else {
-    setSuccess(email);
+    error.textContent = "";
   }
-};
+}
