02cefe54d9f1fe715c86a7568971fe89448c49e9
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-03-26 21:07:15 +0200

    ....

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function errorMessage() {
   var error = document.getElementById("error");
   if (isNaN(document.getElementById("number").value)) {
     // Changing content and color of content
-    error.textContent = "Please enter a valid number";
+    error.textContent = "Please provide a valid email";
     error.style.color = "red";
   } else {
     error.textContent = "";
