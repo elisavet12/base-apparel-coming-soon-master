f5900d55839c07c8cff47804c280554e07eb49be
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-03-27 20:32:15 +0300

    ....

--- index.js
+++ index.js
@@ -9,13 +9,37 @@
 //   }
 // }
 
-function errorMessage() {
-  var error = document.getElementById("error");
-  if (isNaN(document.getElementById("number").value)) {
-    // Changing content and color of content
-    error.textContent = "Please provide a valid email";
-    error.style.color = "red";
+// function errorMessage() {
+//   var error = document.getElementById("error");
+//   if (isNaN(document.getElementById("number").value)) {
+//     // Changing content and color of content
+//     error.textContent = "Please provide a valid email";
+//     error.style.color = "red";
+//   } else {
+//     error.textContent = "";
+//   }
+// }
+
+let errorIcon = document.querySelector(".error-img");
+let warning = document.querySelector(".error");
+let button = document.querySelector(".submit");
+let input = document.querySelector("input");
+let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+button.addEventListener("click", (event) => {
+  if (input.value.match(regex)) {
+    errorIcon.style.display = "none";
+    warning.innerText = "";
+    input.value = "";
+    input.style.outline = "yellowgreen solid 2px";
   } else {
-    error.textContent = "";
+    errorIcon.style.display = "block";
+    warning.innerText = "Please provide a valid email";
+    input.style.outline = "red solid 2px";
   }
-}
+});
+input.addEventListener("keydown", (keyboardEvent) => {
+  if (keyboardEvent.key === "Enter") {
+    button.click();
+  }
+});
