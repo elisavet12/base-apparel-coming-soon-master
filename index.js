74664eee6b311eab93a251f0a939c2a6d7c0e16e
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-05-02 01:20:16 +0300

    ...

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// function validEmail() {
-//   var mail = document.getElementById("mail").value; // where `mail` is id of your input form
-//   var email = /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/;
-
-//   if (mail.match(email)) {
-//     alert("Your Email : " + mail);
-//   } else {
-//     alert("Invalid Email !!!");
-//   }
-// }
-
-// function errorMessage() {
-//   var error = document.getElementById("error");
-//   if (isNaN(document.getElementById("number").value)) {
-//     // Changing content and color of content
-//     error.textContent = "Please provide a valid email";
-//     error.style.color = "red";
-//   } else {
-//     error.textContent = "";
-//   }
-// }
-
 let errorIcon = document.querySelector(".error-img");
 let warning = document.querySelector(".error");
 let button = document.querySelector(".submit");
@@ -30,7 +8,6 @@ button.addEventListener("click", (event) => {
   if (input.value.match(regex)) {
     errorIcon.style.display = "none";
     warning.innerText = "";
-    input.value = "";
     input.style.outline = "yellowgreen solid 2px";
   } else {
     errorIcon.style.display = "block";
