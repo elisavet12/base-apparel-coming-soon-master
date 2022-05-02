36f7ea00a63f1f53aee7087ac5ca0e41b87add5d
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-05-02 20:04:40 +0300

    ....

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let errorIcon = document.querySelector(".error-img");
+let errorIcon = document.querySelector("img.error-img");
+let errorActive = document.querySelector("img.error-img-active");
 let warning = document.querySelector("p.error");
 let button = document.querySelector(".submit");
 let input = document.querySelector("input");
@@ -7,14 +8,16 @@ let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 button.addEventListener("click", (event) => {
   if (input.value.match(regex)) {
     errorIcon.style.display = "none";
+
     warning.innerText = "";
-    input.style.outline = "yellowgreen solid 2px";
+    // input.style.outline = "yellowgreen solid 2px";
   } else {
     errorIcon.style.display = "block";
     // warning.innerText = "Please provide a valid email"; //
     // document.getElementsByClassName.style.display = "block";//
-    input.style.outline = "red solid 2px";
+    input.style.outline = "hsl(0, 93%, 68%) solid 1px";
     warning.style.display = "block";
+    errorActive.classList.add("error-img-active");
   }
 });
 input.addEventListener("keydown", (keyboardEvent) => {
